fdf2325b7ca70da8d1cf1324086fb8b67de5fb3a
sayaka <sayaka.19980426@gamil.com>
2020-09-13 05:26:08 +0000

    デプロイ修正

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,29 @@
+class FavoritesController < ApplicationController
+    def index
+        @favorite_topics = current_user.favorite_topics
+    end
+    
+    def create
+        favorite = Favorite.new
+        favorite.user_id = current_user.id
+        favorite.topic_id = params[:topic_id]
+        
+        if favorite.save
+            redirect_to topics_path, success: "お気に入り登録しました。"
+        else
+            redirect_to topics_path, danger: "お気に入り登録に失敗しました。"
+        end
+    end
+    
+    def destroy
+        favorite = Favorite.find_by(topic_id: params[:topic_id], user_id: current_user.id)
+        if favorite.destroy
+            redirect_to topics_path, success: "お気に入りを取り消しました。"
+        else
+            redirect_to topics_path, danger: "お気に入りの取り消しに失敗しました。"
+        end
+     
+    end
+end
+
+
